41d68fb2865df1353c2188721cdb27d1b8d6c47f
krishpranav <krisna.pranav@gmail.com>
2021-06-12 12:43:09 +0530

    [+] config lua [+]

---
+++ nextdisplay/config.lua
@@ -0,0 +1,30 @@
+return {
+	device = "desktop",
+
+
+	keymap = "default.lua",
+
+	layer_distance = 0.2,
+	near_layer_sz = 1280,
+	display_density = 33,
+	curve = 0.9,
+	layer_falloff = 0.9,
+	animation_speed = 30,
+	preview_w = 2560,
+	preview_h = 1440,
+	prefix = "",
+
+	scale_y = -1.0,
+
+	control_path = "control",
+	allow_ipc = true,
+
+	terminal_font = "hack.ttf",
+	terminal_font_sz = 24,
+	terminal_opacity = 0.9,
+
+	icon_set = "default",
+
+	meta_1 = "LMETA", 
+	meta_2 = "RMETA"
+};
