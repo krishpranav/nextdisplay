f9eec53f49c904caba4ad423f38943df65aa60d6
krishpranav <krisna.pranav@gmail.com>
2021-06-12 12:44:25 +0530

    [+] Added some values [+]

--- nextdisplay/gconf.lua
+++ nextdisplay/gconf.lua
@@ -1,3 +1,25 @@
 local log = suppl_add_logfn("config");
 local applname = string.lower(APPLID);
 
+LBL_YES = "yes";
+LBL_NO = "no";
+LBL_FLIP = "toggle";
+LBL_BIND_COMBINATION = "Press and hold the desired combination, %s to Cancel";
+LBL_BIND_KEYSYM = "Press and hold single key to bind keysym %s, %s to Cancel";
+LBL_BIND_COMBINATION_REP = "Press and hold or repeat- press, %s to Cancel";
+LBL_UNBIND_COMBINATION = "Press and hold the combination to unbind, %s to Cancel";
+LBL_METAGUARD = "Query Rebind in %d keypresses";
+LBL_METAGUARD_META = "Rebind (meta keys) in %.2f seconds, %s to Cancel";
+LBL_METAGUARD_BASIC = "Rebind (basic keys) in %.2f seconds, %s to Cancel";
+LBL_METAGUARD_MENU = "Rebind (global menu) in %.2f seconds, %s to Cancel";
+LBL_METAGUARD_TMENU = "Rebind (target menu) in %.2f seconds, %s to Cancel";
+
+HC_PALETTE = {
+	"\\#efd469",
+	"\\#43abc9",
+	"\\#cd594a",
+	"\\#b5c689",
+	"\\#f58b4c",
+	"\\#ed6785",
+	"\\#d0d0d0",
+};
