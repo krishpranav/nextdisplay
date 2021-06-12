293ad22d4accd2151189cff00aa9664875e499d8
krishpranav <krisna.pranav@gmail.com>
2021-06-12 11:16:51 +0530

    [+] lg 100 [+]

---
+++ nextdisplay/devices/lg100.lua
@@ -0,0 +1,9 @@
+return {
+    display = 'Dell',
+    oversample_w = 1.0,
+    oversample_h = 1.0,
+    distortion_model = "none",
+    display_rotate = "cw90ccw90",
+    with = 1920,
+    height = 1680,
+};
