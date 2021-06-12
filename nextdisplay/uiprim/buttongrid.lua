03c839c80ebfef3e93b3d85d25fde78867940c9b
krishpranav <krisna.pranav@gmail.com>
2021-06-12 12:24:50 +0530

    [+] over function [+]

--- nextdisplay/uiprim/buttongrid.lua
+++ nextdisplay/uiprim/buttongrid.lua
@@ -9,3 +9,13 @@ end
 local function own(ctx, vid)
     return ctx.vids[vid] ~= nil;
 end
+
+
+local function over(ctx, vid)
+    local tbl = ctx.vids[vid];
+    image_color(vid,
+        tbl.color_hi[1] * 255,
+        tbl.color_hi[2] * 255,
+        tbl.color_hi[3] * 255
+    );
+end
