d0363e0d37b73d1ec9c120f80537e4958afab852
krishpranav <krisna.pranav@gmail.com>
2021-06-12 11:22:06 +0530

    [+] simulated [+]

---
+++ nextdisplay/devices/simulated.lua
@@ -0,0 +1,12 @@
+return {
+    distortion_model = "basic",
+    headless = true,
+    disable_vrbridge = true,
+    oversample_w = 1.0,
+    oversample_h = 1.0,
+    with = VRESW,
+    height = VRESW,
+    bindings = {
+
+    },
+};
